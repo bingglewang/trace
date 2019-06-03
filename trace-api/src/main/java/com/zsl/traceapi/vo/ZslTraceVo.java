ae71bee8142fc2243afa3dca88119550abd8f3e0
bingglewang <1342102300@qq.com>
2019-06-03 15:51:49 +0800

    分页信息处理

--- trace-api/src/main/java/com/zsl/traceapi/vo/ZslTraceVo.java
+++ trace-api/src/main/java/com/zsl/traceapi/vo/ZslTraceVo.java
@@ -1,6 +1,5 @@
 package com.zsl.traceapi.vo;
 
-import com.zsl.tracedb.model.AllianceBusiness;
 import com.zsl.tracedb.model.Merchant;
 import lombok.Data;
 
@@ -10,7 +9,7 @@ import java.util.Date;
 public class ZslTraceVo {
     private Integer traceId;  //追溯id
     private String traceCodeNumber;  //追溯码批次号
-    private String merchantName; //商家名称
+    private Merchant merchant; //商家信息
     private Integer traceApplyType; //申请类型
     private Integer traceHandleStatus; // 申请处理状态
     private String  companyName;  // 公司名称
