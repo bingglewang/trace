efac0a6640d6ab87201346bec8ebc06eb93e2e4e
binggleW <1342102300@qq.com>
2020-05-20 12:31:10 +0800

    1、kafka 修改

--- trace-api/src/main/java/com/zsl/traceapi/config/kafka/consumer/TraceCodeConsumerKafka.java
+++ trace-api/src/main/java/com/zsl/traceapi/config/kafka/consumer/TraceCodeConsumerKafka.java
@@ -1,10 +1,27 @@
 package com.zsl.traceapi.config.kafka.consumer;
 
+import com.zsl.traceapi.config.kafka.producer.TraceCodeProducerKafka;
+import com.zsl.traceapi.dao.ZslTraceSubcodeDao;
+import com.zsl.traceapi.dto.TracePreSidInsertParam;
+import com.zsl.traceapi.dto.TraceSubcodeInsertParam;
 import com.zsl.traceapi.util.MyThread;
+import com.zsl.traceapi.util.RandomUtil;
+import com.zsl.traceapi.util.SpringContextUtil;
 import com.zsl.traceapi.util.TraceSidThread;
+import com.zsl.tracedb.mapper.ZslCurrentEIndexMapper;
+import com.zsl.tracedb.mapper.ZslTraceMapper;
+import com.zsl.tracedb.mapper.ZslTraceSidMapper;
+import com.zsl.tracedb.model.*;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 追溯子码监听消费者
@@ -12,18 +29,144 @@ import org.springframework.stereotype.Component;
 @Component
 public class TraceCodeConsumerKafka {
 
+    private Logger logger = LoggerFactory.getLogger(TraceCodeConsumerKafka.class);
+
+    @Autowired
+    private ZslTraceSubcodeDao zslTraceSubcodeDao;
+
+    @Autowired
+    private ZslTraceSidMapper zslTraceSidMapper;
+
+    @Autowired
+    private ZslTraceMapper zslTraceMapper;
+
+    @Autowired
+    private TraceCodeProducerKafka traceCodeProducerKafka;
+
+    @Autowired
+    private ZslCurrentEIndexMapper zslCurrentEIndexMapper;
+
     @KafkaListener(topics = "traceInsert")
     public void handle(ConsumerRecord<?, ?> record){
         String traceCodeNumber = record.value().toString();
         try {
             Long zslTradeSid = Long.parseLong(traceCodeNumber);
-            new TraceSidThread(zslTradeSid).start();
+            //原来
+            //new TraceSidThread(zslTradeSid).start();
+            //现在
+            run(zslTradeSid);
         }catch (Exception e){
-            new MyThread(traceCodeNumber).start();
+            //原来
+            //new MyThread(traceCodeNumber).start();
+            //现在
+            run1(traceCodeNumber);
         }
     }
 
 
+    public void run(Long zslTradeSid){
+        logger.info("要预生成的id:{}", zslTradeSid);
+        //根据id 获取sid记录
+        ZslTraceSid zslTraceSid = zslTraceSidMapper.selectByPrimaryKey(zslTradeSid);
+        if(zslTraceSid != null){
+            Long startIndex = zslTraceSid.getSidStartIndex();
+            Long endIndex = zslTraceSid.getSidEndIndex();
+            List<TracePreSidInsertParam> insertParams = new ArrayList<>();
+            Long currentTimeStamp = System.currentTimeMillis() * 1000;
+            for(Long i = startIndex; i <= endIndex; i++){
+                TracePreSidInsertParam tracePreSidInsertParam = new TracePreSidInsertParam();
+                StringBuffer stringBuffer = new StringBuffer();
+                stringBuffer.append(RandomUtil.getRandNumberCode(4));
+                stringBuffer.append(currentTimeStamp);
+                stringBuffer.append(i + 1);
+                tracePreSidInsertParam.setTraceSid(i);
+                tracePreSidInsertParam.setTraceSubcodeNumber(stringBuffer.toString());
+                insertParams.add(tracePreSidInsertParam);
+                if (insertParams.size() >= 10000) {
+                    zslTraceSubcodeDao.insertCodePreSidBatch(insertParams);
+                    insertParams.clear();
+                }
+            }
+            //循环完
+            if (insertParams.size() > 0) {
+                zslTraceSubcodeDao.insertCodePreSidBatch(insertParams);
+            }
+            //修改生成状态
+            ZslTraceSid sidUpdate = new ZslTraceSid();
+            sidUpdate.setId(zslTradeSid);
+            sidUpdate.setSidPreCreate((byte)1); //修改为已经生成完
+            zslTraceSidMapper.updateByPrimaryKeySelective(sidUpdate);
+            logger.info("已经预生成完的id:{}", zslTradeSid);
+        }
+    }
+
+
+
+    public void run1(String traceCodeNumber) {
+        try {
+            logger.info("要生成的批次号:{}", traceCodeNumber);
+            //判断是否已经有，有则删掉之前的
+            ZslTraceSubcode zslTraceSubcode = zslTraceSubcodeDao.selectByCodeNumber(traceCodeNumber);
+            if (zslTraceSubcode != null) {
+                int j = zslTraceSubcodeDao.deleteByCodeNumber(traceCodeNumber);
+                logger.info(zslTraceSubcode + "的删除结果:{}", j);
+            }
+
+
+            //拿到追溯信息
+            ZslTraceExample zslTraceExample = new ZslTraceExample();
+            ZslTraceExample.Criteria criteria = zslTraceExample.createCriteria();
+            criteria.andTraceCodeNumberEqualTo(traceCodeNumber);
+            List<ZslTrace> zslTraceList = zslTraceMapper.selectByExample(zslTraceExample);
+            if (!(CollectionUtils.isEmpty(zslTraceList))) {
+                Long count = zslTraceList.get(0).getTraceApplyCount();
+                List<TraceSubcodeInsertParam> insertParams = new ArrayList<>();
+                Long currentTimeStamp = System.currentTimeMillis() * 1000;
+
+                ZslCurrentEIndex zslCurrentEIndex = zslCurrentEIndexMapper.selectByPrimaryKey(1);
+                Long sidStart = zslCurrentEIndex.getCurrentEIndex() + 1;
+
+                for (int i = 0; i < count; i++) {
+                    StringBuffer stringBuffer = new StringBuffer();
+                    TraceSubcodeInsertParam subcodeInsertParam = new TraceSubcodeInsertParam();
+                    stringBuffer.append(RandomUtil.getRandNumberCode(4));
+                    stringBuffer.append(currentTimeStamp);
+                    stringBuffer.append(i + 1);
+                    subcodeInsertParam.setTraceIndex(new Long(i + 1));
+                    subcodeInsertParam.setTraceCodeNumber(traceCodeNumber);
+                    subcodeInsertParam.setTraceSubcodeNumber(stringBuffer.toString());
+                    subcodeInsertParam.setTraceSid(sidStart + i);
+                    insertParams.add(subcodeInsertParam);
+                    if (insertParams.size() >= 10000) {
+                        zslTraceSubcodeDao.insertCodeBatch(insertParams);
+                        insertParams.clear();
+                    }
+                }
+                if (insertParams.size() > 0) {
+                    zslTraceSubcodeDao.insertCodeBatch(insertParams);
+                }
+                //修改追溯状态为：已生成Y
+                ZslTrace updateTrace = new ZslTrace();
+                updateTrace.setTraceId(zslTraceList.get(0).getTraceId());
+                updateTrace.setTraceBack3("Y");
+                zslTraceMapper.updateByPrimaryKeySelective(updateTrace);
+                //更改当前电子指针位置
+                ZslCurrentEIndex updateE = new ZslCurrentEIndex();
+                updateE.setId(1);
+                updateE.setCurrentEIndex(zslCurrentEIndex.getCurrentEIndex() + zslTraceList.get(0).getTraceApplyCount());
+                zslCurrentEIndexMapper.updateByPrimaryKeySelective(updateE);
+                //调用生成三维码接口
+                // traceCodeImageProducerKafka.sendMessage(traceCodeNumber);
+            }
+            logger.info("已经完成的批次号:{}", traceCodeNumber);
+        } catch (Exception e) {
+            logger.info("处理失败:{}", traceCodeNumber);
+            try {
+                traceCodeProducerKafka.sendMessage(traceCodeNumber);
+            } catch (Exception e1) {
+            }
+        }
+    }
 
 
 }
--- trace-api/src/main/java/com/zsl/traceapi/config/kafka/consumer/TraceUpdateConsumerKafka.java
+++ trace-api/src/main/java/com/zsl/traceapi/config/kafka/consumer/TraceUpdateConsumerKafka.java
@@ -1,24 +1,124 @@
 package com.zsl.traceapi.config.kafka.consumer;
 
-import com.zsl.traceapi.util.CoreThread;
+import com.alibaba.fastjson.JSONObject;
+import com.zsl.traceapi.dao.ZslTraceSubcodeDao;
+import com.zsl.traceapi.dto.TraceCodeRelation;
+import com.zsl.traceapi.dto.TraceSubcodeUpdateParam;
+import com.zsl.traceapi.dto.TraceSubcodeUpdateParamSid;
+import com.zsl.traceapi.util.SpringContextUtil;
+import com.zsl.tracedb.mapper.ZslTraceMapper;
+import com.zsl.tracedb.model.ZslTrace;
+import com.zsl.tracedb.model.ZslTraceExample;
+import com.zsl.tracedb.model.ZslTracePapper;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class TraceUpdateConsumerKafka {
     private static Logger logger = LoggerFactory.getLogger(TraceUpdateConsumerKafka.class);
+
+    @Autowired
+    private ZslTraceSubcodeDao zslTraceSubcodeDao;
+
+    @Autowired
+    private ZslTraceMapper zslTraceMapper;
 
 
     @KafkaListener(topics = "traceUpdate")
     public void handle(ConsumerRecord<?, ?> record) {
         logger.info("接受到的json字符串:{}", record.value());
         String traceCodeJson = record.value().toString();
-        new CoreThread(traceCodeJson).start();
+        //原来
+        //new CoreThread(traceCodeJson).start();
+        //现在
+        run(traceCodeJson);
     }
 
 
+    /**
+     * 判断是纸质还是电子
+     *
+     * @param traceCodeNumber
+     * @return
+     */
+    private boolean isPapperOrElectric(String traceCodeNumber) {
+        ZslTraceExample zslTraceExample = new ZslTraceExample();
+        ZslTraceExample.Criteria criteria = zslTraceExample.createCriteria();
+        criteria.andTraceCodeNumberEqualTo(traceCodeNumber);
+        List<ZslTrace> zslTraceList = zslTraceMapper.selectByExample(zslTraceExample);
+        if (CollectionUtils.isNotEmpty(zslTraceList) && zslTraceList.get(0).getTraceApplyType() - 1 == 0) {
+            return true;
+        }
+        return false;
+    }
 
+    public void run(String traceCodeJson) {
+        try {
+            TraceCodeRelation traceCodeRelation = JSONObject.parseObject(traceCodeJson, TraceCodeRelation.class);
+            Long fromNumber = traceCodeRelation.getFromNumber();
+            Long toNumber = traceCodeRelation.getToNumber();
+            int goodsId = traceCodeRelation.getGoodsId();
+            int stallId = traceCodeRelation.getStallId();
+            String traceCodeNumber = traceCodeRelation.getTraceCodeNumber();
+
+            //判断是纸质还是电子
+            boolean isPaperType = isPapperOrElectric(traceCodeNumber);
+            Long count = toNumber - fromNumber + 1;
+            if (isPaperType) {
+                List<ZslTracePapper> papperList = traceCodeRelation.getTracePappers();
+                if(papperList != null){
+                    long countTemp = fromNumber;
+                    for(int i = 0;i < papperList.size();i++){
+                        long start = papperList.get(i).getTraceNumStart();
+                        long end = papperList.get(i).getTraceNumEnd();
+                        //关联操作
+                        List<Long> traceCodeIdsPaper = zslTraceSubcodeDao.selectBySidRange(start, end);
+                        List<TraceSubcodeUpdateParamSid> updateSidParams = new ArrayList<>();
+                        for (int j = 0; j < traceCodeIdsPaper.size(); j++) {
+                            TraceSubcodeUpdateParamSid traceSubcodeUpdateParamSid = new TraceSubcodeUpdateParamSid(countTemp, traceCodeNumber, goodsId, stallId, traceCodeIdsPaper.get(j));
+                            updateSidParams.add(traceSubcodeUpdateParamSid);
+                            if(updateSidParams.size() % 1000 == 0){
+                                int m = zslTraceSubcodeDao.updateGoodsAndStallSid(updateSidParams);
+                                updateSidParams.clear();
+                            }
+                            countTemp++;
+                        }
+                        if (updateSidParams.size() > 0) {
+                            zslTraceSubcodeDao.updateGoodsAndStallSid(updateSidParams);
+                        }
+                    }
+                }
+            } else {
+                //电子
+                int totalPage = (int) Math.ceil((double) count / 1000);
+                for (int currentPage = 1; currentPage <= totalPage; currentPage++) {
+                    Long fromIndex = 0L;
+                    Long toIndex = 0L;
+                    fromIndex = new Long((currentPage - 1) * 1000 + fromNumber);
+                    if (currentPage - totalPage == 0) {
+                        toIndex = toNumber;
+                    } else {
+                        toIndex = new Long(currentPage * 1000) + fromNumber;
+                    }
+
+                    List<Long> traceCodeIds = zslTraceSubcodeDao.selectByRange(fromIndex, toIndex, traceCodeNumber);
+                    List<TraceSubcodeUpdateParam> updateParams = new ArrayList<>();
+                    for (int i = 0; i < traceCodeIds.size(); i++) {
+                        TraceSubcodeUpdateParam traceSubcodeUpdateParam = new TraceSubcodeUpdateParam(goodsId, stallId, traceCodeIds.get(i));
+                        updateParams.add(traceSubcodeUpdateParam);
+                    }
+                    int j = zslTraceSubcodeDao.updateGoodsAndStall(updateParams);
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
 }
